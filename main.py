7207b59eaefc4f355758c61fc1ed49cd343beafd
ALZAHRAAQ8 <142684218+ALZAHRAAQ8@users.noreply.github.com>
2023-09-06 19:35:48 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# write your code here
+person = {
+    "name" : "alzahraa",
+    "age" : 17 ,
+    "hobbies" : ["karate and foot ball", 'eat'],
+}
+print(person["name"])
+print(person["age"])
+
+person["age"] = 18
+person["country"] = "kuwait"
+
+print(f"hi my name is {person['name']} , iam from {person['country']}, iam {person['age']} years old ,i hope to be a {person['hobbies']} playre")
+print(f"{person}")
+
+
+person["hobbies"].append("swimming")
+print(person)
+
+
+
+def check_hobbies(person):
+    length = len(person['hobbies']) 
+    if length >= 3:
+        print("wow you are amazing")
+
+check_hobbies(person)
+
